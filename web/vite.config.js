3a85dd63d28ce4df9173dd306bdf618af4c4df71
Ben <benkurut@gmail.com>
2026-03-12 15:14:51 -0400

    set base path to root for custom domain

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/Abide/',
+  // serve from root for both dev and production (custom domain)
+  base: '/',
   plugins: [react()],
   build: {
     minify: 'esbuild',
